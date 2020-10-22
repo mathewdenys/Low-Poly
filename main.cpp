4499e88bc2a6f7b3f982ce13a50573f9da209bc3
Mathew Denys <mathew@denys.nz>
2020-10-22 20:22:12 +1300

    Image load and export

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+#include <opencv2/core/mat.hpp>  // for basic OpenCV structures (cv::Mat, Scalar)
+#include <opencv2/imgcodecs.hpp> // for reading and writing
+
 // main() takes one input argument: the name of the input image
 int main( int argc, char** argv )
 {
     // 1. Load image
+    const std::string inputImageName = argv[1];
+    cv::Mat imgIn = cv::imread(inputImageName,cv::IMREAD_COLOR);
 
     // 2. Extract keypoints
     //  - Edge detection
@@ -13,6 +18,8 @@ int main( int argc, char** argv )
     // 4. Colour polygons
 
     // 5. Export image
+    cv::Mat imgOut = imgIn.clone();
+    cv::imwrite("media/output.jpg",imgOut);
 
 	return 0;
 }
