d6abf50df28517603f788d4f44d685cdff6f77d8
Mathew Denys <mathew@denys.nz>
2020-10-22 20:18:31 +1300

    Initial commit; program outline

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// main() takes one input argument: the name of the input image
+int main( int argc, char** argv )
+{
+    // 1. Load image
+
+    // 2. Extract keypoints
+    //  - Edge detection
+    //  - Some randomisation for aesthetic appeal
+
+    // 3. Create polygons
+    //  - Delaunay triangulation
+
+    // 4. Colour polygons
+
+    // 5. Export image
+
+	return 0;
+}
